419ed90d1132a2ae821c2eeb6236b8a3ae41b49d
Bryan te Beek <bryantebeek@gmail.com>
2013-04-10 18:21:11 +0200

    Clean up metadata code

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          'Apache 2.0'
 description      'Installs/Configures MailCatcher'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.3'
-recipe            "mailcatcher", "Installs MailCatcher"
-recipe            "mailcatcher::php", "Setup PHP to use MailCatcher using sendmail_path"
+recipe           'mailcatcher', 'Installs MailCatcher'
+recipe           'mailcatcher::php', 'Setup PHP to use MailCatcher using sendmail_path'
