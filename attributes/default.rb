e75416ac09448ad18e8f7badad495465e8108ec2
Zach Segal <zsegal@activecom.net>
2014-03-19 22:49:17 -0400

    Add multiple attribute, default to false

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,3 +24,5 @@ default['mailcatcher']['http-port'] = 1080
 
 default['mailcatcher']['smtp-ip'] = '127.0.0.1'
 default['mailcatcher']['smtp-port'] = 1025
+
+default['mailcatcher']['multiple'] = false
